0b0449da47d799522ec83d9610fb2cdc2ee09318
snbadeaux@gmail.com <snbadeaux@gmail.com>
2023-09-07 19:17:48 -0500

    adding changes to table sql file

--- crowdfunding_db_table_setup.sql
+++ crowdfunding_db_table_setup.sql
@@ -1,7 +1,22 @@
 --Create table schema
+CREATE TABLE "category" (
+    "category_id" VARCHAR(50) PRIMARY KEY  NOT NULL,
+    "category" VARCHAR(50)   NOT NULL,
+);
+CREATE TABLE "contacts" (
+    "contact_id" INT PRIMARY KEY  NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+);
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(50) PRIMARY KEY NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
+);
 CREATE TABLE "campaign" (
     "cd_id" INT PRIMARY KEY  NOT NULL,
     "contact_id" INT   NOT NULL,
+        FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
     "company_name" VARCHAR(50)   NOT NULL,
     "description" VARCHAR(225)   NOT NULL,
     "goal" NUMERIC(10,2)   NOT NULL,
@@ -13,32 +28,16 @@ CREATE TABLE "campaign" (
     "launched_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
     "category_id" VARCHAR(50)   NOT NULL,
+        FOREIGN KEY (category_id) REFERENCES category(category_id),
     "subcategory_id" VARCHAR(50)   NOT NULL,
-    );
-
-CREATE TABLE "category" (
-    "category_id" VARCHAR(50) PRIMARY KEY  NOT NULL,
-    "category" VARCHAR(50)   NOT NULL,
+        FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
 );
 
-CREATE TABLE "contacts" (
-    "contact_id" INT PRIMARY KEY  NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL,
-);
 
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(50) PRIMAR KEY NOT NULL,
-    "subcategory" VARCHAR(50)   NOT NULL,
-);
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contract_id" FOREIGN KEY("contract_id")
-REFERENCES "contacts" ("contact_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
-REFERENCES "category" ("category_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "subcategory" ("subcategory_id");
+
+
+
 
